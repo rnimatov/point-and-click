78f4b8b6747af5596fb2ad02b11c902d2f0b832e
rnimatov <r.nimatov@sintlucasedu.nl>
2023-03-22 10:41:34 +0100

    1

--- js/index.js
+++ js/index.js
@@ -1,2 +1,15 @@
-document.getElementById("mainTitle").innerText = "Point and click adventure"
-document.getElementById("mainTitle").style.textAlign = "center";
+document.getElementById("mainTitle").innerText = "Point and click adventure";
+
+const mainCharacter = document.getElementById("mainCharacter");
+const gameScreen = document.getElementById("gameWindow");
+
+
+gameWindow.onClick = function(){
+
+    var rect = gameWindow.getBoundingClientRect();
+    var x = e.clientX - rect.left; //xposition of the element
+    var Y = e.clientY - rect.top; //yposition of the element
+    mainCharacter.style.left = x + "px";
+    mainCharacter.style.top = y + "px";
+    //console.log(x);
+}
