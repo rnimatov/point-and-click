6bf4351fcb58481701e70b63e60659872975efbc
rnimatov <r.nimatov@sintlucasedu.nl>
2023-03-22 12:54:58 +0100

    1

--- js/index.js
+++ js/index.js
@@ -3,8 +3,8 @@ document.getElementById("mainTitle").innerText = "Point and click adventure";
 console.offsetCharacter = 16;
 
 const mainCharacter = document.getElementById("mainCharacter");
-const gameScreen = document.getElementById("gameWindow");
-
+const gameWindow = document.getElementById("gameWindow");
+const characterAudio = 
 
 gameWindow.onClick = function(e){
 
@@ -13,6 +13,25 @@ gameWindow.onClick = function(e){
     var Y = e.clientY - rect.top; //yposition of the element
     mainCharacter.style.left = x - offsetCharacter + "px";
     mainCharacter.style.top = y - offsetCharacter + "px";
+
+    console.log(e.target.id);
+
+    switch(e.target.id) {
+        case "door1":
+            characterAudio.play();
+            // sometjhign insert here
+            console.log("LLLLLLLLL");
+                break;
+            case "door2":
+                //something insert here
+                console.log("yessssssss");
+                break;
+            default:
+            //do somethng when it doesnt work
+            console.log("nooooooo");
+            break;
+
+    }
     //console.log(x);
 
     // put in css on main character transition: all 1s ease-in-out; 
