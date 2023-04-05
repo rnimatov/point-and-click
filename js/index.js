0c99b55bca93ce1777540832282673d39d423ad4
rnimatov <r.nimatov@sintlucasedu.nl>
2023-04-05 19:35:06 +0200

    1

--- js/index.js
+++ js/index.js
@@ -1,38 +1,100 @@
 document.getElementById("mainTitle").innerText = "Point and click adventure";
 
-console.offsetCharacter = 16;
+const offsetCharacter = 8;
+
+const sec = 1000;
 
 const mainCharacter = document.getElementById("mainCharacter");
 const gameWindow = document.getElementById("gameWindow");
-const characterAudio = 
+const characterAudio = document.getElementById("characterAudio");
+const mainCharacterSpeech = document.getElementById("mainCharacterSpeech");
 
-gameWindow.onClick = function(e){
+const counterSpeech = document.getElementById("counterSpeech");
+const counterAudio = document.getElementById("counterAudio");
+const counterCharacter = document.getElementById("counterCharacter");
 
-    var rect = gameWindow.getBoundingClientRect();
-    var x = e.clientX - rect.left; //xposition of the element
-    var Y = e.clientY - rect.top; //yposition of the element
-    mainCharacter.style.left = x - offsetCharacter + "px";
-    mainCharacter.style.top = y - offsetCharacter + "px";
+gameWindow.onclick = function (e) {
+    if (mainCharacterSpeech.style.opacity == 0 && counterSpeech.style.opacity == 0) {
+        var rect = gameWindow.getBoundingClientRect();
+        var x = e.clientX - rect.left; //x position within the element.
+        var y = e.clientY - rect.top;  //y position within the element.
+        mainCharacter.style.left = x - offsetCharacter + "px";
+        mainCharacter.style.top = y - offsetCharacter + "px";
 
-    console.log(e.target.id);
-
-    switch(e.target.id) {
-        case "door1":
-            characterAudio.play();
-            // sometjhign insert here
-            console.log("LLLLLLLLL");
+        switch (e.target.id) {
+            case "door1":
+                //something insert here
+                showSpeech(mainCharacterSpeech, characterAudio, "this is door one.<br> And it's locked dummy...");
                 break;
             case "door2":
                 //something insert here
-                console.log("yessssssss");
+                showSpeech(mainCharacterSpeech, characterAudio, "noboby is home...<br> Come back later..");
+                break;
+            case "statue":
+                //something insert here
+                showSpeech(mainCharacterSpeech, characterAudio, "What a borring statue...");
+                setTimeout(function () { counterCharacter.style.opacity = 1; }, 4 * sec);
+                setTimeout(showSpeech, 4 * sec, counterSpeech, counterAudio, "No you are borring..");
+                setTimeout(showSpeech, 8 * sec, mainCharacterSpeech, characterAudio, "Ow sorry I didn't know you could speak.");
                 break;
             default:
-            //do somethng when it doesnt work
-            console.log("nooooooo");
-            break;
-
+                // do something when it doesn't have a case
+                hideSpeech();
+                break;
+        }
     }
-    //console.log(x);
-
-    // put in css on main character transition: all 1s ease-in-out; 
 }
+
+function showSpeech(targetBubble, targetAudio, dialogue) {
+    //trigger speech bubble and audio
+    targetBubble.style.opacity = 1;
+    targetBubble.innerHTML = dialogue;
+    targetAudio.currentTime = 0;
+    targetAudio.play();
+    //stop after 4 seconds the dialogue bubble and audio
+    setTimeout(hideSpeech, 4 * sec, targetBubble, targetAudio);
+}
+
+function hideSpeech(targetBubble, targetAudio) {
+    targetBubble.style.opacity = 0;
+    targetBubble.innerHTML = "...";
+    targetAudio.pause();
+}
+
+function getItem(itemName, itemId) {
+    inventory.push(itemName);
+    console.log(itemName);
+}
+
+function checkItem(item) {
+    return inventory.includes(item);
+}
+
+function showItem(itemName, itemId) {
+
+    let listItem = document.createElement("li");
+
+    listItem.id = itemId;
+
+    listItem.appendChild(document.createTextNode(itemName));
+
+    inventoryList.appendChild(listItem);
+}
+
+function removeItem(itemName, itemId) {
+    //remove item in Array
+    inventory = inventory.filter(function (newInventory) {
+        return newInventory !== itemName;
+    });
+
+    //removes list element in HTML
+    document.getElementById(itemId).remove();
+}
+
+getItem("bread", "bread");
+getItem("cold milk", "coldMilk");
+getItem("eggs", "eggs");
+removeItem("cold milk", "coldMilk");
+
+
+
